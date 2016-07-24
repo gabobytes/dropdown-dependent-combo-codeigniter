39264ba2ba8fda3f6b5524bb70fe24c4b39cdebe
Gabriel Melo <gabrielmeloarteaga@gmail.com>
2016-07-23 23:36:38 -0500

    loading.gif added

--- application/views/demo/demo_view.php
+++ application/views/demo/demo_view.php
@@ -1,38 +1,54 @@
 <?php $this->load->view('masterpage/head'); ?>
-
-
  <script>
- $(document).ready(function($) {
-	
+ $(document).ready(function($) {	
  	$("#country").change(function(event) {
  		var idcountry = this.value;
+ 
+ 		$("#divLoading").removeClass('hidden');
 
  		//clear state and city before load data
  		$("#state").empty();
  		$("#city").empty();
 
  		//load states with get method
- 		$.get('<?php echo site_url();?>demo/getStates/'+idcountry, function(data) {
- 			$("#state").html(data);
+ 		$.get('<?php echo site_url();?>demo/getStates/'+idcountry)
+	 		.done(function(data){
+	 			$("#state").html(data);
+	 		}).fail(function(){
+	 			alert('error loading data');
+	 		}).always(function(){
+	 			//hide loading.gif
+	 			$("#divLoading").addClass('hidden');
  		});
-
-
- 	
  	$("#state").change(function(event) {
 		var idstate = this.value;
 
-		$.get('<?php echo site_url();?>demo/getCities/'+idstate, function(data) {
-			$("#city").html(data);
-		});
+		$("#divLoading").removeClass('hidden');
+
+ 		//load cities with get method
+ 		$.get('<?php echo site_url();?>demo/getCities/'+idstate)
+	 		.done(function(data){
+	 			$("#city").html(data);
+	 		}).fail(function(){
+	 			alert('error loading data');
+	 		}).always(function(){
+	 			//hide loading.gif
+	 			$("#divLoading").addClass('hidden');
+ 		})
  	});
 
  	});
 
  });//document ready
  </script>
-
 <div class="row">
 	<h1>Dropdown dependent - CI and Bootstrap</h1>	
+
+	<div class="row container hidden" id="divLoading">
+		<div class="col-md-3">
+			<img src="<?php echo site_url();?>assets/img/loading.gif" alt="">Loading...
+		</div>
+	</div>
 	<div class="col-md-8">
 		<div class="form-group">
 		<label for="country">Country</label>
